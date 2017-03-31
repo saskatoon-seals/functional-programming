28fd9eed74654599d10f3387751b73e2ceae0d89
Saskatoon Seals <saskatoon.seals@gmail.com>
2017-03-31 08:44:33 -0600

    Optional monad (reinvention of the wheel) (#27)
    * Optional Monad - src and test code
    
    * Optional Monad - added doxygen comments

---
+++ Optionals/src/vcommondemo/Optional.java
@@ -0,0 +1,137 @@
+package vcommondemo;
+
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Reinvention of an Optional monad with minimum functionality that still satisfies the
+ * monad laws:
+ *  1. Left Identity
+ *  2. Right Identity
+ *  3. Associativity
+ *
+ * @param <T> - value to wrap into the context of a monad
+ */
+public class Optional<T> {
+  private T value;
+
+  private Optional() {
+    this(null);
+  }
+
+  private Optional(T value) {
+    this.value = value;
+  }
+
+  //************************************************************************************************
+  //                                            Monad APIs
+  //************************************************************************************************
+
+  /**
+   * Map operation (Functor)
+   *
+   * Maps a function over a functor's value
+   *
+   * @param mapper - function to map over
+   * @return Optional with a mapped value
+   */
+  public <R> Optional<R> map(Function<T, R> mapper) {
+    return execute(
+        () -> Optional.of(mapper.apply(value))
+    );
+  }
+
+  /**
+   * Compose apply (Applicative functor)
+   *
+   * NOTE: Can't be used in a fluent (applicative style) way
+   *
+   * @param functor - functor with a function as it's value
+   * @return Optional with a mapped value
+   */
+  public <R> Optional<R> composeApply(Optional<Function<T, R>> functor) {
+    if (functor.isEmpty()) {
+      return empty();
+    }
+
+    return map(functor.get());
+  }
+
+  /**
+   * Apply operation (Applicative functor)
+   *
+   * NOTE: Compiler doesn't know weather a functor has a function or data - it can't prevent
+   * a runtime error of trying to feed data to data
+   *
+   * @param functor - Optional functor
+   * @return - new Optional functor
+   * @exception ClassCastException - if the current optional contains data instead of a function
+   */
+  @SuppressWarnings("unchecked")
+  public <U, R> Optional<R> apply(Optional<U> functor) {
+    return execute(
+        () -> functor.map((Function<U, R>) value)
+    );
+  }
+
+  /**
+   * FlatMap or bind operation (Monad)
+   *
+   * @param mapper - mapper that accepts a value and returns an optional
+   * @return optional with preserved context (emptiness)
+   */
+  public <R> Optional<R> flatMap(Function<T, Optional<R>> mapper) {
+    return execute(
+        () -> mapper.apply(value)
+    );
+  }
+
+  //************************************************************************************************
+  //                                      Non-monad APIs
+  //************************************************************************************************
+
+  public T get() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+
+    return value;
+  }
+
+  public boolean isEmpty() {
+    return value == null;
+  }
+
+  //************************************************************************************************
+  //                                       Static APIs
+  //************************************************************************************************
+
+  public static <T> Optional<T> empty() {
+    return new Optional<>();
+  }
+
+  /**
+   * Unary return operation
+   *
+   * Wraps a given generic value into an optional with a minimum context
+   *
+   * @param value - value to wrap
+   * @return value wrapped in an optional
+   */
+  public static <T> Optional<T> of(T value) {
+    return new Optional<>(value);
+  }
+
+  //************************************************************************************************
+  //                                       Helper methods
+  //************************************************************************************************
+
+  private <R> Optional<R> execute(Supplier<Optional<R>> supplier) {
+    if (isEmpty()) {
+      return empty();
+    }
+
+    return supplier.get();
+  }
+}
---
+++ Optionals/test/vcommondemo/OptionalTest.java
@@ -0,0 +1,219 @@
+package vcommondemo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+
+public class OptionalTest {
+
+  //************************************************************************************************
+  //                                      Map tests (Functor)
+  //************************************************************************************************
+
+  @Test
+  public void mapReturnsFunctorWithConcatenatedWords() {
+    assertEquals(
+        "Ales loves functors.",
+        Optional.of("Ales")
+                .map(value -> value.concat(" loves functors."))
+                .get()
+    );
+  }
+
+  @Test
+  public void mapReturnsFunctorWithSum() {
+    assertEquals(
+        new Integer(3),
+        Optional.of(13)
+                .map(value -> value - 10)
+                .get()
+    );
+  }
+
+  @Test
+  public void returnsEmptyWhenMappingOverEmptyFunctor() {
+    Optional<Double> x = Optional.empty();
+
+    Optional<Double> y = x.map(value -> 0.5 * value);
+
+    assertTrue(x.isEmpty());
+    assertTrue(y.isEmpty());
+  }
+
+  //************************************************************************************************
+  //                                      ComposeApply tests (Applicative)
+  //************************************************************************************************
+
+  @Test
+  public void appliesAdditionToFunctorWithNumber() {
+    assertEquals(
+        new Integer(12),
+        Optional.of(9)
+                .composeApply(Optional.of(value -> value + 3))
+                .get()
+    );
+  }
+
+  @Test
+  public void appliesEmptyFunctionPreservesEmptiness() {
+    assertTrue(
+        Optional.of(9)
+                .composeApply(Optional.empty())
+                .isEmpty()
+    );
+  }
+
+  @Test
+  public void appliesFunctionOnEmptyApplicativePreservesEmptiness() {
+    assertTrue(
+        Optional.<Integer>empty()
+                .composeApply(Optional.of(value -> value + 3))
+                .isEmpty()
+    );
+  }
+
+  //pure(x -> y -> y / x) <*> a <*> b
+  @Test
+  public void chainsApplicationOfFunctionOnTwoFunctors() {
+    assertEquals(
+        new Double(2.5),
+        Optional.of(10.0)
+                .composeApply(
+                    Optional.of(4)
+                            .composeApply(
+                                Optional.of(x -> y -> y / x)
+                            )
+                )
+                .get()
+    );
+  }
+
+  @Test
+  public void chainsApplicationOfFunctionOnEmptyAndNonEmptyFunctor() {
+    Optional<Integer> a = Optional.empty();
+    Optional<Double> b = Optional.of(10.0);
+
+    assertTrue(
+        b.composeApply(
+            a.composeApply(
+                Optional.of(x -> y -> y / x)
+            )
+         )
+         .isEmpty()
+    );
+  }
+
+  @Test
+  public void chainsApplicationOfFunctionOnNonEmptyAndEmptyFunctor() {
+    Optional<Integer> a = Optional.of(4);
+    Optional<Double> b = Optional.empty();
+
+    assertTrue(
+        b.composeApply(
+            a.composeApply(
+                Optional.of(x -> y -> y / x)
+            )
+         )
+         .isEmpty()
+    );
+  }
+
+  //************************************************************************************************
+  //                                      Apply tests (Applicative)
+  //************************************************************************************************
+
+  @Test
+  public void appliesSingleFunctionOnFunctor() {
+    Function<Integer, Integer> mapper = value -> value + 10;
+
+    assertEquals(
+        new Integer(110),
+        Optional.of(mapper)
+                .apply(Optional.of(100))
+                .get()
+    );
+  }
+
+  @Test
+  public void chainsFuntionApplicationsOnTwoFunctors() {
+    Function<Integer, Function<Integer, Integer>> mapper = a -> b -> a - b;
+
+    assertEquals(
+        new Integer(190),
+        Optional.of(mapper)
+                .apply(Optional.of(200))
+                .apply(Optional.of(10))
+                .get()
+    );
+  }
+
+  @Test
+  public void chainsFunctionApplicationOnEmptyAndNonEmptyFunctors() {
+    Function<Integer, Function<Integer, Integer>> mapper = a -> b -> a - b;
+
+    assertTrue(
+        Optional.of(mapper)
+                .apply(Optional.empty())
+                .apply(Optional.of(10))
+                .isEmpty()
+    );
+  }
+
+  @Test(expected=ClassCastException.class)
+  public void throwsWhenApplyingFunctorOnNonApplicative() {
+    Function<Integer, Integer> mapper = value -> value + 10;
+
+    Optional.of(mapper)
+            .apply(Optional.of(100))  //Applicative becomes a normal functor (with data value)
+            .apply(Optional.of(100)); //At this point the optional doesn't contain a function anymore
+  }
+
+  //************************************************************************************************
+  //                                      flatMap tests (Monad)
+  //************************************************************************************************
+
+  @Test
+  public void flatMapReturnsOptionalWithNumberDivision() {
+    assertEquals(
+        new Double(2.5),
+        Optional.of(10.0)
+                .flatMap(value -> Optional.of(value / 4))
+                .get()
+    );
+  }
+
+  @Test
+  public void flatMapConcatenatesStrings() {
+    assertEquals(
+        "Ales loves monads.",
+        Optional.of("Ales")
+                .flatMap(value -> Optional.of(value.concat(" loves monads.")))
+                .get()
+    );
+  }
+
+  @Test
+  public void chainsFlatMapOperations() {
+    assertEquals(
+        new Integer(20),
+        Optional.of(1)
+                .flatMap(value -> Optional.of(value + 10))
+                .flatMap(value -> Optional.of(value + 9))
+                .get()
+    );
+  }
+
+  @Test
+  public void flatMapPreservesContext() {
+    assertTrue(
+        Optional.of("Ales")
+                .flatMap(value -> Optional.of(value.concat(" Ursic")))
+                .flatMap(value -> Optional.<String>empty())
+                .flatMap(value -> Optional.of(value.concat(" loves monads.")))
+                .isEmpty()
+    );
+  }
+}
